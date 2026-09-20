e90023d09a488ed42b2d00b7770e357cfae91a3b
lylna <2750595466@qq.com>
2026-09-20 17:06:56 +0800

    用户user模块的开发 全局权限校验AOP 开发用户注册、登陆、获取用户信息、用户注销等功能

--- src/main/java/com/lyl/aicodegenerator/AiCodeGeneratorApplication.java
+++ src/main/java/com/lyl/aicodegenerator/AiCodeGeneratorApplication.java
@@ -1,10 +1,12 @@
 package com.lyl.aicodegenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.lyl.aicodegenerator.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 
 public class AiCodeGeneratorApplication {
